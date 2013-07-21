a57b16f81b7ec46d5ec45113e65878df792f7afb
HU Pili <hpl1989@gmail.com>
2013-07-21 18:34:55 +0800

    make pypi package

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='hupili',
+      version='0.0.1',
+      description='utility collected by hupili',
+      author='Pili Hu',
+      author_email='me@hupili.net',
+      url='https://github.com/hupili/py-hupili',
+      packages=['hupili']
+     )
