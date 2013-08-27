700ddf02cd9933f76cf89558ac04250eae729c7c
HU Pili <hpl1989@gmail.com>
2013-08-28 00:14:28 +0800

    formalize

--- hupili/mq.py
+++ hupili/mq.py
@@ -1,28 +1,34 @@
 import pymongo
 from pymongo import MongoClient
 
-client = MongoClient()
-db = client.test
-q = db.queue
+class MongoQueue(object):
+    """docstring for MongoQueue"""
+    def __init__(self, host='localhost', port=27017, database='mq', collection='queue'):
+        super(MongoQueue, self).__init__()
+        self.host = host
+        self.port = port
+        self.database = database
+        self.collection = collection
+        self.client = MongoClient(host, port)
+        self.queue = self.client[database][collection]
 
-def enqueue(d):
-    return q.insert({'doc': d})
+    def enqueue(self, d):
+        return self.queue.insert({'doc': d})
 
-def dequeue():
-    d = q.find_and_modify(query={}, sort={'_id': 1}, remove=True)
-    if d:
-        return d['doc']
-    else: 
-        return None
+    def dequeue(self):
+        d = self.queue.find_and_modify(query={}, sort={'_id': 1}, remove=True)
+        if d:
+            return d['doc']
+        else: 
+            return None
 
-def empty():
-    return q.find().count() == 0
+    def empty(self):
+        return self.queue.find().count() == 0
 
-map(lambda i: enqueue(i), range(0,5))
-
-#print [i for i in q.find()]
-
-while not empty():
-    d = dequeue()
-    if not d is None:
-        print d
+if __name__ == '__main__':
+    q = MongoQueue()
+    map(lambda i: q.enqueue(i), range(0,5))
+    while not q.empty():
+        d = q.dequeue()
+        if not d is None:
+            print d
