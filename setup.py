65ec8db27215404f528d002cda34e18238d64826
HU Pili <hpl1989@gmail.com>
2013-08-28 11:14:37 +0800

    new version, add MQ

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='hupili',
-      version='0.1.3',
+      version='0.2.0',
       description='utility collected by hupili',
       author='Pili Hu',
       author_email='me@hupili.net',
